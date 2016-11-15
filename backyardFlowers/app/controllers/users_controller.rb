24d89141ea40200e0b4c68c3811c463ec4433ea0
Sanaz <sanaz.jamloo@gmail.com>
2016-11-15 11:34:20 -0800

    Create users_controller

--- backyardFlowers/app/controllers/users_controller.rb
+++ backyardFlowers/app/controllers/users_controller.rb
@@ -1,8 +1,61 @@
 class UsersController < ApplicationController
 
-
   def set_users
     @user = User.find(params[:id])
   end
+
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {status: 200, message: "Successful Sign Up"}
+    else
+      render json: {status: 422, user: user.errors}
+    end
+  end
+
+  def index
+    users = User.all
+    render json: {status: 200, users: users}
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
