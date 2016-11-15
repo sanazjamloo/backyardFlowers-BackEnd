96bb637570ad43a9870837ff1e6e2c72def29266
Sanaz <sanaz.jamloo@gmail.com>
2016-11-15 10:59:14 -0800

    Add routes to routes.rb

--- backyardFlowers/config/routes.rb
+++ backyardFlowers/config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show, :destroy, :index] do
+  resources :flowers, only: [:create, :show, :index, :update, :destroy]
+  collection do
+    post '/login', to: 'users#login'
+  end
 end
+end
+
+#Prefix Verb   URI Pattern                           Controller#Action
+#user_flowers GET    /users/:user_id/flowers(.:format)     flowers#index
+#             POST   /users/:user_id/flowers(.:format)     flowers#create
+# user_flower GET    /users/:user_id/flowers/:id(.:format) flowers#show
+#             PATCH  /users/:user_id/flowers/:id(.:format) flowers#update
+#             PUT    /users/:user_id/flowers/:id(.:format) flowers#update
+#             DELETE /users/:user_id/flowers/:id(.:format) flowers#destroy
+# login_users POST   /users/login(.:format)                users#login
+#       users GET    /users(.:format)                      users#index
+#             POST   /users(.:format)                      users#create
+#        user GET    /users/:id(.:format)                  users#show
+#             DELETE /users/:id(.:format)                  users#destroy
