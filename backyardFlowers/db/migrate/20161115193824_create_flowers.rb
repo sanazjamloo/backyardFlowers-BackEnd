9bb585cd3fb547c19abc2858d31d031725538090
Sanaz <sanaz.jamloo@gmail.com>
2016-11-15 11:45:39 -0800

    Create migrations for flowers and users

---
+++ backyardFlowers/db/migrate/20161115193824_create_flowers.rb
@@ -0,0 +1,14 @@
+class CreateFlowers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :flowers do |t|
+      t.string :name
+      t.datetime :date_planted
+      t.string :perennial_or_annual
+      t.string :sun
+      t.string :water
+      t.text :memo
+
+      t.timestamps
+    end
+  end
+end
