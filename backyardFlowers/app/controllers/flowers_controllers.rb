35809337605e7418ee74045cd6db3d029879098e
Sanaz <sanaz.jamloo@gmail.com>
2016-11-15 20:31:31 -0800

    Add flowers_controller.rb

--- backyardFlowers/app/controllers/flowers_controllers.rb
+++ backyardFlowers/app/controllers/flowers_controllers.rb
@@ -5,29 +5,29 @@ class FlowersController < ApplicationController
    @user = User.find(params[:user_id])
  end
 
-   # GET /flowers
-  def index
-   flowers = Flower.all
-
-   render json: flowers
+ # POST /flowers
+  def create
+    flower = Flower.new(flower_params)
+    flower.user_id = @user.id
+   #  puts(flower_params)
+    if flower.save
+      render json: {status: 200, message:'Created a new flower!'}
+    else
+      render json: {status: 422, flower: flower.errors}
+    end
   end
 
-  # GET /flowers/1
-  def show
-    flower = Flower.find(params[:id])
-    render json: flower
-  end
+     # GET /flowers
+    def index
+     flowers = @user.flower
+     render json: {status: 200, flowers: flowers}
+    end
 
-  # POST /flowers
-   def create
-     flower = Flower.new(flower_params)
-    #  puts(flower_params)
-     if flower.save
-       render json: flower, status: :created, location: flower
-     else
-       render json: flower.errors, status: :unprocessable_entity
-     end
-   end
+    # GET /flowers/1
+      def show
+        flower = @user.flower.find(params[:id])
+        render json: {status: 200, flower: flower}
+      end
 
     # PATCH/PUT /flowers/1
     def update
@@ -43,14 +43,19 @@ class FlowersController < ApplicationController
     def destroy
       flower = Flower.find(params[:id])
       flower.destroy
-
       render json: {status: 204}
     end
+
+      def destroy_all
+    @user = User.find(params[:user_id])
+    Flower.where(:user_id => @user).destroy_all
+    render json: {status: 204}
+   end
 
     private
     #Only allow a trusted param "white list" through.
      def flower_params
         params.required(:flower).permit(:name, :date_planted, :perennial_or_annual, :sun, :water, :memo)
-      end
-
     end
+
+end
